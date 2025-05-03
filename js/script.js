4c0e4fe89e4690c81aac9e2a9137cc3d5dc25d3c
Oleh Barbashov <teosofio@gmail.com>
2025-05-03 21:21:45 +0300

    Handling a past date in a countdown timer

--- js/script.js
+++ js/script.js
@@ -43,14 +43,23 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Timer
 
-    const deadline = '2025-05-29';
+    const deadline = '2020-05-29';
 
     function getTimeRemaining(endtime) {
-        const total = Date.parse(endtime) - Date.parse(new Date()),
-              days = Math.floor(total / (1000 * 60 * 60 * 24)),
-              hours = Math.floor((total / (1000 * 60 * 60)) % 24),
-              minutes = Math.floor((total / (1000 * 60)) % 60),
-              seconds = Math.floor((total / 1000) % 60);
+        let days, hours, minutes, seconds;
+        const total = Date.parse(endtime) - Date.parse(new Date());
+
+        if (total <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
+            days = Math.floor(total / (1000 * 60 * 60 * 24));
+            hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+            minutes = Math.floor((total / (1000 * 60)) % 60);
+            seconds = Math.floor((total / 1000) % 60);
+        }
 
         return {
             'total': total,
