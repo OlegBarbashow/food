aad2495e1963db0c885fd1c7eaa9430987361347
Oleh Barbashov <teosofio@gmail.com>
2025-05-26 15:58:55 +0300

    Created a calculator.

--- js/script.js
+++ js/script.js
@@ -424,4 +424,98 @@ window.addEventListener('DOMContentLoaded', () => {
             dots[slideIndex - 1].style.opacity = '1';
         });
     });
+
+    // Calc
+
+    const result = document.querySelector('.calculating__result span');
+    let sex = 'female', height, weight, age, ratio = 1.375;
+
+    function calcTotal() {
+        if (!sex || !height || !weight || !age || !ratio) {
+            result.textContent = '____';
+            return;
+        }
+
+        if (sex === 'female') {
+            result.textContent = String(Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio));
+        } else {
+            result.textContent = String(Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio));
+        }
+    }
+
+    calcTotal();
+
+    function getStaticInformation(parentSelector, activeClass) {
+        const elements = document.querySelectorAll(`${parentSelector} div`);
+
+        elements.forEach(elem => {
+            elem.addEventListener('click', (evt) => {
+                const target = evt.target;
+
+                if (target.getAttribute('data-ratio')) {
+                    ratio = +target.getAttribute('data-ratio');
+                    console.log(true);
+                } else {
+                    sex = target.getAttribute('id');
+                }
+
+                console.log(ratio, sex);
+
+                elements.forEach(elem => {
+                    elem.classList.remove(activeClass);
+                });
+                target.classList.add(activeClass);
+
+                calcTotal();
+            });
+        });
+
+
+    }
+
+    getStaticInformation('#gender', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+
+    function getDynamicInformation(selector) {
+        const input = document.querySelector(selector);
+
+        input.addEventListener('input', () => {
+            switch (input.getAttribute('id')) {
+                case 'height':
+                    height = +input.value;
+                    break;
+                case 'weight':
+                    weight = +input.value;
+                    break;
+                case 'age':
+                    age = +input.value;
+                    break;
+            }
+            calcTotal();
+        });
+    }
+
+    getDynamicInformation('#height');
+    getDynamicInformation('#weight');
+    getDynamicInformation('#age');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
